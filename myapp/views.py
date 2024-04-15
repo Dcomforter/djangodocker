385ab96d9cbbb6b6ad717a24101adb896271bc2e
Israel Okuneye <ibo7@msstate.edu>
2024-04-15 12:32:13 -0500

    Updated views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    print("My name is Israel, and this a Python Application I just created using Django Web Framework")
-    return HttpResponse("Hello World! This is Israel the DevOps Master")
+    task = "My name is Israel, and this a Python Application I just created using Django Web Framework"
+    return HttpResponse(task, "Hello World! This is Israel the DevOps Master")
