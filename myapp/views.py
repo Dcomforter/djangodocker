dc877c0c9c5a2a83a8d83f6ed52b4490e71499c6
Israel Okuneye <ibo7@msstate.edu>
2024-05-09 15:37:17 -0500

    Updated views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    task = "This is Israel, the DevOps Master, presenting you with a Dockerized Python Application created using Django Web Framework. I have also created a CICD pipeline using Jenkins. Check it OUT "
+    task = "This is Israel, the DevOps Master"
     
     return HttpResponse(task)
